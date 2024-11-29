5835baf3625c3d4ddc812369cbe8334a7d7d6aea
qadhara1996 <bespyana96@gmail.com>
2024-11-29 19:40:59 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -19,7 +19,7 @@
 
 // переменные
 
-// var, let, const
+// var, let, const - var устаревший
 
 // let num = 5; //присвоение
 // let str = '5';
@@ -49,26 +49,63 @@
 // \ - экранирование символа (обычный символ)
 // `` - бэктик - многострочный текст - template string (шаблонная строка) втавка ${имя переменной которую нужно вставить, прим. `Привет дорошой ${str}`}
 // ${имя переменной} - интерполяция - динамическая вставка в текст значений переменных (работает только с бэктиками ``)
-let str = 'boy\'s ball';
-let str2 = "boy's ball";
-let str3 = `boy's ball`;
+// let str = 'boy\'s ball';
+// let str2 = "boy's ball";
+// let str3 = `boy's ball`;
 
-let helloMessage = 'Привет, дорогой ${str}';
+// let helloMessage = 'Привет, дорогой ${str}';
 
-const GLOBAL_SETTINGS = 3.14; //большие буквы и подчеркивание - глобальные и важные постоянные значения
+// const GLOBAL_SETTINGS = 3.14; //большие буквы и подчеркивание - глобальные и важные постоянные значения
 
 // V8 - движок
 
 //{} - литерал объекта
-const obj = {
-    age: 22,
-    name: 'John',
-    gender: 'male',
-    isStudent: true,
+// const obj = {
+//     age: 22,
+//     name: 'John',
+//     gender: 'male',
+//     isStudent: true,
 
-}
+// }
 
 // let str = 'aifja'; => литерал строки
 // let newString = new String('hflla')
 
-console.log(obj); //проверка, для пушить - удалить - логировать в консоли в браузера, для отладки
+// console.log(obj); //проверка, для пушить - удалить - логировать в консоли в браузера, для отладки
+//console.log(str);
+
+
+// Модальное окно
+// prompt, alert, confirm
+
+// prompt('massage', 'defaultAnswer')
+
+// const userName = prompt('введите ваше имя', 'John'); //1 - вопрос, 2- ответ по умолчанию
+// console.log(userName);
+
+// alert('message') - уведомление с кнопкой ок
+// alert('ok?');
+
+// confirm
+// const result = confirm('massage');
+//___________________________________________________________________________________________________________________________
+
+// //1
+// console.log('Запускаем таймер');
+// const timer1 = alert(5);
+// const timer2 = alert(4);
+// const timer3 = alert(3);
+// const timer4 = alert(2);
+// const timer5 = alert(1);
+// const timer6 = alert(0);
+// console.log('Обратный отсчет закончен');
+
+// //2
+// const myName = 'Yana';
+// const mySurname = 'Bespyataya';
+// const myFavoriteDrink = 'Beer';
+// const myFavoriteAnimal = 'Cats';
+// console.log(myName);
+// console.log(mySurname);
+// console.log(myFavoriteDrink);
+// console.log(myFavoriteAnimal); 
